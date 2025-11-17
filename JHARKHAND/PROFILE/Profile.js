a3fb924aaeda09b4f2dbf2184b0b537bf9b68a08
arpita-sinha-14 <sinhaaarpita147@gmail.com>
2025-11-18 01:39:58 +0530

    Added all the pages

---
+++ JHARKHAND/PROFILE/Profile.js
@@ -0,0 +1,7 @@
+document.querySelector('.edit-btn').addEventListener('click', () => {
+  alert("Edit Profile feature coming soon!");
+});
+
+document.querySelector('.save-btn').addEventListener('click', () => {
+  alert("Profile Updated Successfully!");
+});
